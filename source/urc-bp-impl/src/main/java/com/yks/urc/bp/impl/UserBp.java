c431841185656a3a7d209679bb80a2dc1e0d770f
linwanxian <1140380776@qq.com>
2018-06-07 16:57:20 +0800

    编写同步信息

--- source/urc-bp-impl/src/main/java/com/yks/urc/bp/impl/UserBp.java
+++ source/urc-bp-impl/src/main/java/com/yks/urc/bp/impl/UserBp.java
@@ -1,18 +1,50 @@
 package com.yks.urc.bp.impl;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPath;
+import com.yks.urc.fw.HttpUtility;
+import com.yks.urc.vo.UserVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserBp {
-	/**
-	 * token 请求地址
-	 */
-	private static final  String GET_TOKEN ="https://userinfo.youkeshu.com/api/get_token";
-	/**
-	 * 获取钉钉信息地址
-	 */
-	private  static  final  String DING_DING ="http://cf.youkeshu.com/pages/viewpage.action?pageId=2556124";
-	public void SynUserFromUserInfo() {
-		//1.只调用UserInfo接口，同步UserInfo数据
-	}
+    private static Logger logger = LoggerFactory.getLogger(UserBp.class);
+    /**
+     * token 请求地址
+     */
+    private static final String GET_TOKEN = "https://userinfo.youkeshu.com/api/get_token";
+    /**
+     * 获取钉钉信息地址
+     */
+    private static final String DING_DING = "https://userinfo.youkeshu.com/api/UserList/";
+    private static String username = "linwanxian";
+    private static String password = "linwx123";
+
+    /**
+     * 同步数据
+     */
+    public void SynUserFromUserInfo() {
+        //1.请求token
+        JSONObject object = new JSONObject();
+        object.put("username", username);
+        object.put("password", password);
+        try {
+            String accessToken = HttpUtility.sendPost(GET_TOKEN, object.toJSONString());
+            System.out.println(accessToken);
+            //2.只调用UserInfo接口，同步UserInfo数据
+            UserVO userVO = new UserVO();
+           // String token =  JSONPath.contains(accessToken,"$.token");
+           // String userInfo = HttpUtility.sendGet(DING_DING,token);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        UserBp userBp = new UserBp();
+        userBp.SynUserFromUserInfo();
+    }
 }
