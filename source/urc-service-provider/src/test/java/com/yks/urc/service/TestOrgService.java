3e043abb764b199814e52d7ebdfe949002cffc47
lvcr <lvchangrong@youkeshu.com>
2018-06-11 14:43:07 +0800

    修改单元测试

--- source/urc-service-provider/src/test/java/com/yks/urc/service/TestOrgService.java
+++ source/urc-service-provider/src/test/java/com/yks/urc/service/TestOrgService.java
@@ -1,25 +1,16 @@
 package com.yks.urc.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.yks.urc.ServiceProviderApplication;
 import com.yks.urc.dingding.client.DingApiProxy;
-import com.yks.urc.entity.Organization;
 import com.yks.urc.service.api.IOrganizationService;
 import com.yks.urc.service.api.IPersonService;
-import com.yks.urc.vo.OrgVO;
-import com.yks.urc.vo.PersonVO;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = ServiceProviderApplication.class)
-public class TestOrgService {
+/*@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServiceProviderApplication.class)*/
+public class TestOrgService extends BaseServiceTest {
 	
 	private static Logger LOG = LoggerFactory.getLogger(TestOrgService.class);
 	
