713e414deea695c935f6c690ec75d4334d470ce0
lvcr <lvchangrong@youkeshu.com>
2018-06-11 17:34:11 +0800

    修改urc-taskSchedule 为基础框架形式

---
+++ source/urc-taskScheduler/src/main/java/com/yks/urc/TaskSchedulerStarter.java
@@ -0,0 +1,17 @@
+package com.yks.urc;
+
+import com.yks.urc.constant.Constant;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//@SpringBootApplication
+//@EnableScheduling
+public class TaskSchedulerStarter {
+	private static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(TaskSchedulerStarter.class);
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				new String[]{Constant.TASKSERVICE_STARTER_CONFIG});
+		LOGGER.info("UrcService Server start...");
+	}
+}
