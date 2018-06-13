13774522694aa7f45ae2702bab37744582ad9f5e
panyun <panyun@youkeshu.com>
2018-06-13 09:03:40 +0800

    Seq生成

---
+++ source/urc-bp-api/src/main/java/com/yks/urc/seq/bp/api/ISeqBp.java
@@ -0,0 +1,75 @@
+package com.yks.urc.seq.bp.api;
+
+/**
+ * 序列生成
+ * 
+ * @author panyun@youkeshu.com
+ * @date 2018年6月13日 上午8:37:47
+ * 
+ */
+public interface ISeqBp {
+	long getNextSeq(String strSeqName);
+
+	/**
+	 * urc_role.role_id
+	 * 
+	 * @return
+	 * @author panyun@youkeshu.com
+	 * @date 2018年6月13日 上午8:39:11
+	 */
+	long getNextRoleId();
+
+	/**
+	 * urc_data_rule.data_rule_id
+	 * 
+	 * @return
+	 * @author panyun@youkeshu.com
+	 * @date 2018年6月13日 上午8:42:06
+	 */
+	long getNextDataRuleId();
+
+	/**
+	 * urc_data_rule_templ.templ_id
+	 * 
+	 * @return
+	 * @author panyun@youkeshu.com
+	 * @date 2018年6月13日 上午8:42:28
+	 */
+	long getNextDataRuleTemplId();
+
+	/**
+	 * urc_data_rule_sys.data_rule_sys_id
+	 * 
+	 * @return
+	 * @author panyun@youkeshu.com
+	 * @date 2018年6月13日 上午8:42:45
+	 */
+	long getNextDataRuleSysId();
+
+	/**
+	 * urc_sql.sql_id
+	 * 
+	 * @return
+	 * @author panyun@youkeshu.com
+	 * @date 2018年6月13日 上午8:43:03
+	 */
+	long getNextSqlId();
+
+	/**
+	 * urc_auth_way.auth_way_id
+	 * 
+	 * @return
+	 * @author panyun@youkeshu.com
+	 * @date 2018年6月13日 上午8:43:21
+	 */
+	long getNextAuthWayId();
+
+	/**
+	 * urc_data_rule_obj.data_rule_obj_id
+	 * 
+	 * @return
+	 * @author panyun@youkeshu.com
+	 * @date 2018年6月13日 上午8:43:45
+	 */
+	long getNextDataRuleObjId();
+}
---
+++ source/urc-bp-impl/src/main/java/com/yks/urc/seq/bp/impl/SeqBpImpl.java
@@ -0,0 +1,57 @@
+package com.yks.urc.seq.bp.impl;
+
+import org.springframework.stereotype.Component;
+import org.springframework.test.annotation.Commit;
+
+import com.yks.distributed.cache.core.AtomicSequenceGenerator;
+import com.yks.urc.fw.StringUtility;
+import com.yks.urc.seq.bp.api.ISeqBp;
+
+@Component
+public class SeqBpImpl implements ISeqBp {
+
+	public long getNextSeq(String strSeqName) {
+		String seqName = strSeqName;
+		String strSeq = AtomicSequenceGenerator.generateSequenceStr(seqName, 5);
+		System.out.println(strSeq);
+		String lSeq = strSeq.substring(seqName.length() + 8);
+		long lNow = System.currentTimeMillis();
+		long uuid = Long.parseLong(String.format("%s%s", lNow, lSeq));
+		return uuid;
+	}
+
+	public long getNextRoleId() {
+		return getNextSeq("roleId");
+	}
+
+	@Override
+	public long getNextDataRuleId() {
+		return getNextSeq("dataRuleId");
+	}
+
+	@Override
+	public long getNextDataRuleTemplId() {
+		return getNextSeq("dataRuleTemplId");
+	}
+
+	@Override
+	public long getNextDataRuleSysId() {
+		return getNextSeq("dataRuleSysId");
+	}
+
+	@Override
+	public long getNextSqlId() {
+		return getNextSeq("sqlId");
+	}
+
+	@Override
+	public long getNextAuthWayId() {
+		return getNextSeq("authWayId");
+
+	}
+
+	@Override
+	public long getNextDataRuleObjId() {
+		return getNextSeq("dataRuleObjId");
+	}
+}
