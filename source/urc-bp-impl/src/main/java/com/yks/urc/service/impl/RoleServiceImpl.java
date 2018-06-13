c24b34328a75f37a29fae2e61844a9ef1926ebef
oujie <oujie1990@foxmail.com>
2018-06-13 18:53:25 +0800

    完善代码

--- source/urc-bp-impl/src/main/java/com/yks/urc/service/impl/RoleServiceImpl.java
+++ source/urc-bp-impl/src/main/java/com/yks/urc/service/impl/RoleServiceImpl.java
@@ -6,6 +6,7 @@ import com.yks.urc.entity.UserInfoDO;
 import com.yks.urc.mapper.IRoleMapper;
 import com.yks.urc.mapper.IRolePermissionMapper;
 import com.yks.urc.mapper.IUserRoleMapper;
+import com.yks.urc.seq.bp.api.ISeqBp;
 import com.yks.urc.service.api.IRoleService;
 
 import com.yks.urc.vo.*;
@@ -17,7 +18,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.management.relation.Role;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -43,6 +46,9 @@ public class RoleServiceImpl implements IRoleService {
 
     @Autowired
     private IUserRoleMapper userRoleMapper;
+
+    @Autowired
+    private ISeqBp seqBp;
 
     /**
      * Description:
@@ -150,7 +156,7 @@ public class RoleServiceImpl implements IRoleService {
      * @see
      */
     @Override
-    public RoleDO getRoleByRoleId(Integer roleId) {
+    public RoleDO getRoleByRoleId(String roleId) {
         RoleDO roleDO = roleMapper.getRoleByRoleId(roleId);
         return roleDO;
     }
@@ -287,7 +293,7 @@ public class RoleServiceImpl implements IRoleService {
     /**
      * Description:
      * 1、复制角色
-     *
+     *  复制角色将创建一个新的角色，并将原角色的权限自动授予给新角色
      * @param :
      * @return:
      * @auther: lvcr
@@ -295,14 +301,42 @@ public class RoleServiceImpl implements IRoleService {
      * @see
      */
     @Override
-    public void copyRole(String strUserName, String newRoleName, String sourceRoleId) {
+    @Transactional(rollbackFor = Exception.class)
+    public void copyRole(String operator, String newRoleName, String sourceRoleId) {
         /*非admin用户只能管理自己创建的角色*/
+        RoleDO roleDO = getRoleInfo(operator, newRoleName, sourceRoleId);
+        //复制角色信息
+        roleDO.setRoleId(seqBp.getNextRoleId());
+        roleDO.setRoleName(newRoleName);
+        roleMapper.insert(roleDO);
         /*复制对应的角色权功能限关系*/
+        List<RoleDO> rolePermission = getRolePermission(Arrays.asList(sourceRoleId));
+//        rolePermission.stream().forEach(role -> {
+//            role.getPermissionDO()
+//        });
+    }
+
+    /**
+     *
+     * 判断当前操作者权限,再获取被复制角色信息
+     * 1.管理员用户可以复制所有角色信息
+     * 2.普通用户只能复制自己创建的角色信息
+     */
+    private RoleDO getRoleInfo(String operator, String newRoleName, String sourceRoleId){
+        if (roleMapper.checkDuplicateRoleName(newRoleName, null)){
+            throw new RuntimeException("角色名已存在");
+        }
+        //判断当前被复制角色是否为当前用户创建的角色
+        RoleDO roleDO = roleMapper.getRoleByRoleId(sourceRoleId);
+        //判断当前用户是否为管理员用户
+        if (roleMapper.isAdminAccount(operator) || operator.equals(roleDO.getCreateBy())){
+            return roleDO;
+        }
+        throw new RuntimeException("普通用户只能复制自己创建的角色信息");
     }
 
     @Override
     public ResultVO<Integer> checkDuplicateRoleName(String operator, String newRoleName, String roleId) {
-        int count = roleMapper.selectCountByRoleName(newRoleName, roleId);
-        return VoHelper.getSuccessResult(count > 0 ? 1 : 0);
+        return VoHelper.getSuccessResult(roleMapper.checkDuplicateRoleName(newRoleName, roleId)?1:0);
     }
 }
