08cf830a7e17189df8f02766b51894f5ace75b8f
lvcr <lvchangrong@youkeshu.com>
2018-06-13 13:58:03 +0800

    完善代码

--- source/urc-service-api/src/main/java/com/yks/urc/motan/service/api/IUrcService.java
+++ source/urc-service-api/src/main/java/com/yks/urc/motan/service/api/IUrcService.java
@@ -7,54 +7,82 @@ import com.yks.urc.vo.ResultVO;
 import com.yks.urc.vo.UserVO;
 
 public interface IUrcService {
-	/**
-	 * 同步数据
-	 * @param curUser
-	 * @return
-	 */
-	String syncUserInfo(UserVO curUser);
+    /**
+     * 同步数据
+     *
+     * @param curUser
+     * @return
+     */
+    String syncUserInfo(UserVO curUser);
 
-	/**
-	 * 登陆校验：用户名密码检验
-	 * @param curUser
-	 * @param authUser
-	 * @return
-	 * @author panyun@youkeshu.com
-	 * @date 2018年6月6日 下午12:22:35
-	 */
-	String login(Map<String,String> map);
+    /**
+     * 登陆校验：用户名密码检验
+     *
+     * @param curUser
+     * @param authUser
+     * @return
+     * @author panyun@youkeshu.com
+     * @date 2018年6月6日 下午12:22:35
+     */
+    String login(Map<String, String> map);
 
-	
-	/**
-	 * 手动触发“同步钉钉部门及人员”信息
-	 * @param str
-	 * @return
-	 */
-	String syncDingOrgAndUser();
-	
-	
-	
-	/**
-	 * 根据DingOrgId获取人员信息含有分页
-	 * @param str
-	 * @return
-	 */
-	String getUserByDingOrgId(String params);
-	
-	
-	/**
-	 * 组织架构搜索用户
-	 * @param params
-	 * @return
-	 */
-	String getUserByUserInfo(String params);
-	
-	/**
-	 * 组织结构树
-	 * @param params
-	 * @return
-	 */
-	String getAllOrgTree();
 
+    /**
+     * 手动触发“同步钉钉部门及人员”信息
+     *
+     * @param str
+     * @return
+     */
+    String syncDingOrgAndUser();
+
+
+    /**
+     * 根据DingOrgId获取人员信息含有分页
+     *
+     * @param str
+     * @return
+     */
+    String getUserByDingOrgId(String params);
+
+
+    /**
+     * 组织架构搜索用户
+     *
+     * @param params
+     * @return
+     */
+    String getUserByUserInfo(String params);
+
+    /**
+     * 组织结构树
+     *
+     * @param params
+     * @return
+     */
+    String getAllOrgTree();
+
+    /**
+     * 快速分配数据权限模板给用户
+     *
+     * @param jsonStr
+     * @return
+     */
+    String assignDataRuleTempl2User(String jsonStr);
+
+    /**
+     * 根据templId获取数据权限模板
+     *
+     * @param jsonStr
+     * @return
+     */
+    String getDataRuleTemplByTemplId(String jsonStr);
+
+    /**
+     * 获取数据权限模板
+     *
+     * @param jsonStr
+     * @return
+     */
+    String getDataRuleTempl(String jsonStr);
 
 }
--- source/urc-service-provider/src/main/java/com/yks/urc/motan/service/impl/UrcServiceImpl.java
+++ source/urc-service-provider/src/main/java/com/yks/urc/motan/service/impl/UrcServiceImpl.java
@@ -4,10 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.yks.urc.entity.Person;
 import com.yks.urc.fw.StringUtility;
 import com.yks.urc.motan.service.api.IUrcService;
-import com.yks.urc.service.api.IOrganizationService;
-import com.yks.urc.service.api.IPersonService;
-import com.yks.urc.service.api.IRoleService;
-import com.yks.urc.service.api.IUserService;
+import com.yks.urc.service.api.*;
 import com.yks.urc.vo.DataRuleTemplVO;
 import com.yks.urc.vo.PersonVO;
 import com.yks.urc.vo.ResultVO;
@@ -19,63 +16,106 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class UrcServiceImpl implements IUrcService {
 
-	@Autowired
-	private IUserService userService;
+    @Autowired
+    private IUserService userService;
 
-	@Autowired
-	private IRoleService roleService;
-	
-	@Autowired
-	private IPersonService personService;
-	
-	
-	@Autowired
-	private IOrganizationService organizationService;
+    @Autowired
+    private IRoleService roleService;
 
-	@Override
-	public String syncUserInfo(UserVO curUser) {
-		return StringUtility.toJSONString_NoException(userService.syncUserInfo(curUser));
-	}
+    @Autowired
+    private IPersonService personService;
 
-	@Override
-	public String login(Map<String, String> map) {
-		UserVO authUser = new UserVO();
-		authUser.userName = map.get("userName");
-		authUser.pwd = map.get("pwd");
-		authUser.ip = map.get("ip");
-		// UserVO curUser, UserVO authUser
-		return StringUtility.toJSONString_NoException(userService.login(authUser));
-	}
-	
-	@Override
-	public String syncDingOrgAndUser() {
-		return StringUtility.toJSONString_NoException(personService.SynPersonOrgFromDing("hand"));
-	}
 
-	@Override
-	public String getUserByDingOrgId(String params) {
+    @Autowired
+    private IOrganizationService organizationService;
+
+    @Autowired
+    private IDataRuleService dataRuleService;
+
+    @Override
+    public String syncUserInfo(UserVO curUser) {
+        return StringUtility.toJSONString_NoException(userService.syncUserInfo(curUser));
+    }
+
+    @Override
+    public String login(Map<String, String> map) {
+        UserVO authUser = new UserVO();
+        authUser.userName = map.get("userName");
+        authUser.pwd = map.get("pwd");
+        authUser.ip = map.get("ip");
+        // UserVO curUser, UserVO authUser
+        return StringUtility.toJSONString_NoException(userService.login(authUser));
+    }
+
+    @Override
+    public String syncDingOrgAndUser() {
+        return StringUtility.toJSONString_NoException(personService.SynPersonOrgFromDing("hand"));
+    }
+
+    @Override
+    public String getUserByDingOrgId(String params) {
         JSONObject jsonObject = StringUtility.parseString(params);
         int pageNumber = Integer.valueOf(jsonObject.get("pageNumber").toString());
         int pageData = Integer.valueOf(jsonObject.get("pageData").toString());
         PersonVO personVo = StringUtility.parseObject(jsonObject.get("templ").toString(), PersonVO.class);
-		return StringUtility.toJSONString_NoException(personService.getUserByDingOrgId(personVo, pageNumber, pageData));
-	}
+        return StringUtility.toJSONString_NoException(personService.getUserByDingOrgId(personVo, pageNumber, pageData));
+    }
 
-	@Override
-	public String getUserByUserInfo(String params) {
+    @Override
+    public String getUserByUserInfo(String params) {
         JSONObject jsonObject = StringUtility.parseString(params);
         int pageNumber = Integer.valueOf(jsonObject.get("pageNumber").toString());
         int pageData = Integer.valueOf(jsonObject.get("pageData").toString());
         PersonVO personVo = StringUtility.parseObject(jsonObject.get("templ").toString(), PersonVO.class);
-		return StringUtility.toJSONString_NoException(personService.getUserByUserInfo(personVo, pageNumber, pageData));
-	}
+        return StringUtility.toJSONString_NoException(personService.getUserByUserInfo(personVo, pageNumber, pageData));
+    }
 
-	@Override
-	public String getAllOrgTree() {
-		return StringUtility.toJSONString_NoException(organizationService.getAllOrgTree());
-	}
-	
-	
-	
-	
+    @Override
+    public String getAllOrgTree() {
+        return StringUtility.toJSONString_NoException(organizationService.getAllOrgTree());
+    }
+
+    /**
+     * Description: 快速分配数据权限模板给用户
+     *
+     * @param :
+     * @return:
+     * @auther: lvcr
+     * @date: 2018/6/13 13:53
+     * @see
+     */
+    @Override
+    public String assignDataRuleTempl2User(String jsonStr) {
+        return StringUtility.toJSONString_NoException(dataRuleService.assignDataRuleTempl2User(jsonStr));
+    }
+
+    /**
+     * Description: 根据templId获取数据权限模板
+     *
+     * @param :jsonStr
+     * @return: String
+     * @auther: lvcr
+     * @date: 2018/6/13 13:55
+     * @see
+     */
+    @Override
+    public String getDataRuleTemplByTemplId(String jsonStr) {
+        return StringUtility.toJSONString_NoException(dataRuleService.getDataRuleTemplByTemplId(jsonStr));
+    }
+
+    /**
+     * Description: 获取数据权限模板
+     *
+     * @param :jsonStr
+     * @return: String
+     * @auther: lvcr
+     * @date: 2018/6/13 13:56
+     * @see
+     */
+    @Override
+    public String getDataRuleTempl(String jsonStr) {
+        return StringUtility.toJSONString_NoException(dataRuleService.getDataRuleTempl(jsonStr));
+    }
+
+
 }
