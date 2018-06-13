63efbebccbec7a8a70ef25ce751f9899dd408f77
panyun <panyun@youkeshu.com>
2018-06-13 09:16:57 +0800

    增加操作日志Bp

---
+++ source/urc-bp-impl/src/main/java/com/yks/urc/operation/bp/imp/OperationBpImpl.java
@@ -0,0 +1,16 @@
+package com.yks.urc.operation.bp.imp;
+
+import org.springframework.stereotype.Component;
+
+import com.yks.urc.operation.bp.api.IOperationBp;
+
+@Component
+public class OperationBpImpl implements IOperationBp {
+
+	@Override
+	public void addLog(String logger, String msg, Exception ex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
