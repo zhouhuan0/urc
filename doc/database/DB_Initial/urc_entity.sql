c9bc0a88e67491778a56d59a43e3688fb6ca3778
panyun <panyun@youkeshu.com>
2018-06-25 15:16:37 +0800

    add DB_Initial SQL

---
+++ doc/database/DB_Initial/urc_entity.sql
@@ -0,0 +1,19 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : dev.urcenterdbm.kokoerp.com
+Source Server Version : 50632
+Source Host           : dev.urcenterdbm.kokoerp.com:3306
+Source Database       : urcenter
+
+Target Server Type    : MYSQL
+Target Server Version : 50632
+File Encoding         : 65001
+
+Date: 2018-06-25 15:10:49
+*/
+
+-- ----------------------------
+-- Records of urc_entity
+-- ----------------------------
+INSERT INTO `urc_entity` VALUES ('5', 'E_PlatformShopSite', '【平台】【账号】【站点】', 'OMS使用', null, '', null, '');
---
+++ doc/database/DB_Initial/urc_field.sql
@@ -0,0 +1,18 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : dev.urcenterdbm.kokoerp.com
+Source Server Version : 50632
+Source Host           : dev.urcenterdbm.kokoerp.com:3306
+Source Database       : urcenter
+
+Target Server Type    : MYSQL
+Target Server Version : 50632
+File Encoding         : 65001
+
+Date: 2018-06-25 15:11:00
+*/
+-- ----------------------------
+-- Records of urc_field
+-- ----------------------------
+INSERT INTO `urc_field` VALUES ('1', 'F_Platform_Shop_Site', '平台/账号/站点', 'E_PlatformShopSite', 'json', null, '1', null, '', null, '');
