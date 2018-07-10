137680a0d7898e6b8f33b9734187871b37f66b23
panyun <panyun@youkeshu.com>
2018-07-10 08:48:02 +0800

    fix demoClient

--- doc/database/DB_Initial/urc_user_role.sql
+++ doc/database/DB_Initial/urc_user_role.sql
@@ -1,31 +1,38 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : dev.urcenterdbm.kokoerp.com
+Source Server         : test.urcenterdbm.kokoerp.com
 Source Server Version : 50632
-Source Host           : dev.urcenterdbm.kokoerp.com:3306
+Source Host           : test.urcenterdbm.kokoerp.com:3306
 Source Database       : urcenter
 
 Target Server Type    : MYSQL
 Target Server Version : 50632
 File Encoding         : 65001
 
-Date: 2018-06-25 15:12:09
+Date: 2018-07-04 12:12:55
 */
 
 -- ----------------------------
 -- Records of urc_user_role
 -- ----------------------------
-INSERT INTO `urc_user_role` VALUES ('37', '1529649147479000001', 'panyun', '', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('38', '1529649147479000001', 'renmaohua', '', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('39', '1529649147479000001', 'tangfeng', '', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('40', '1529649147479000001', 'tangyong', '', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('41', '1529649147479000001', 'weijie', '', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('42', '1529649147479000001', 'yangbo', 'wujianghui', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('43', '1529649147479000001', 'chenglifu', '', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('44', '1529649147479000001', 'chensi', 'wujianghui', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('45', '1529649147479000001', 'mengyuhua', '', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('68', '1529649147479000001', 'xieyi1', '', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('69', '1529649147479000001', 'xieyi2', '', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('70', '1529649147479000001', 'chensi2', '', null, '', null);
-INSERT INTO `urc_user_role` VALUES ('71', '1529649147479000001', 'liujun', '', null, '', null);
+INSERT INTO `urc_user_role` VALUES ('436', '1529635932385000001', 'wujianghui', 'panyun', '2018-07-03 10:12:10.116', 'panyun', '2018-07-03 10:12:10.116');
+INSERT INTO `urc_user_role` VALUES ('437', '1529635932385000001', 'panyun', 'panyun', '2018-07-03 10:12:10.116', 'panyun', '2018-07-03 10:12:10.116');
+INSERT INTO `urc_user_role` VALUES ('506', '1529635932385000001', 'panyun1', 'panyun', null, '', null);
+INSERT INTO `urc_user_role` VALUES ('511', '1529649147479000001', 'chenbaoxiang1', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('512', '1529649147479000001', 'chensi', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('513', '1529649147479000001', 'chensi2', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('514', '1529649147479000001', 'huangpeiqin', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('515', '1529649147479000001', 'linwanxian', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('516', '1529649147479000001', 'liujun', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('517', '1529649147479000001', 'liuli', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('518', '1529649147479000001', 'mengyuhua', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('519', '1529649147479000001', 'panxi', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('520', '1529649147479000001', 'panyun', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('521', '1529649147479000001', 'renmaohua', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('522', '1529649147479000001', 'tangfeng', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('523', '1529649147479000001', 'tangyong', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('524', '1529649147479000001', 'weijie', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('525', '1529649147479000001', 'xieyi1', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('526', '1529649147479000001', 'xieyi2', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
+INSERT INTO `urc_user_role` VALUES ('527', '1529649147479000001', 'yangbo', 'panyun1', '2018-07-04 08:29:27.074', 'panyun1', '2018-07-04 08:29:27.074');
--- source/democlient/src/main/java/com/yks/demo/controller/MotanUserController.java
+++ source/democlient/src/main/java/com/yks/demo/controller/MotanUserController.java
@@ -20,6 +20,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.jboss.netty.util.internal.StringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpRequest;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.util.ReflectionUtils;
@@ -126,11 +128,18 @@ public class MotanUserController {
         return rslt;
     }
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/stopRequest")
     public ResultVO stopRequest() {
         UserLoginRunnable.isStop = true;
         ResultVO rslt = new ResultVO<>();
-        rslt.msg = String.format("%s", UserLoginRunnable.getStatStr());
+        try {
+            rslt.msg = String.format("%s", UserLoginRunnable.getStatStr());
+        } catch (Exception ex) {
+            logger.error("stopRequest", ex);
+            rslt.msg = ex.getMessage();
+        }
         return rslt;
     }
 }
