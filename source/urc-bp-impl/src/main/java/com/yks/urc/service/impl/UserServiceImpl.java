5b33d9a119669a3735e927275e1641022930c6d0
linwanxian <1140380776@qq.com>
2018-07-12 11:53:33 +0800

    加try

--- source/urc-bp-impl/src/main/java/com/yks/urc/service/impl/UserServiceImpl.java
+++ source/urc-bp-impl/src/main/java/com/yks/urc/service/impl/UserServiceImpl.java
@@ -225,46 +225,50 @@ public class UserServiceImpl implements IUserService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public ResultVO<List<OmsPlatformVO>> getPlatformShopSite(String operator) {
-        List<PlatformDO> platformDOS = platformMapper.selectAll();
-        //装载平台volist
-        List<OmsPlatformVO> omsPlatformVOS = new ArrayList<>();
-        for (PlatformDO platformDO : platformDOS) {
-            OmsPlatformVO omsPlatformVO = new OmsPlatformVO();
-            omsPlatformVO.platformId = platformDO.getPlatformId();
-            omsPlatformVO.platformName = platformDO.getPlatformName();
+       try {
+           List<PlatformDO> platformDOS = platformMapper.selectAll();
+           //装载平台volist
+           List<OmsPlatformVO> omsPlatformVOS = new ArrayList<>();
+           for (PlatformDO platformDO : platformDOS) {
+               OmsPlatformVO omsPlatformVO = new OmsPlatformVO();
+               omsPlatformVO.platformId = platformDO.getPlatformId();
+               omsPlatformVO.platformName = platformDO.getPlatformName();
 
-            List<ShopSiteDO> shopSiteDOS = shopSiteMapper.selectShopSiteByPlatformId(platformDO.getPlatformId());
-            if (shopSiteDOS == null || shopSiteDOS.size() == 0) {
-              continue;
-            } else {
-                //集合都必须先初识化
-                omsPlatformVO.lstShop =new ArrayList<>(shopSiteDOS.size());
-                for (ShopSiteDO shopSiteDO : shopSiteDOS) {
-                        OmsShopVO omsShopVO = new OmsShopVO();
-                        //针对速卖通的
-                        omsShopVO.shopId = shopSiteDO.getSellerId();
-                        omsShopVO.shopName = shopSiteDO.getShop();
-                        //如果站点id为空,则list为空
-                        if ("".equals(shopSiteDO.getSiteId())) {
-                            omsShopVO.lstSite = null;
-                        } else {
-                            OmsSiteVO omsSiteVO = new OmsSiteVO();
-                            omsSiteVO.siteId = shopSiteDO.getSiteId();
-                            //如果站点名称为空,则吧站点id赋值给站点名称
-                            if ("".equals(shopSiteDO.getSiteName())) {
-                                omsSiteVO.siteName = omsSiteVO.siteId;
-                            } else {
-                                omsSiteVO.siteName = shopSiteDO.getSiteName();
-                                omsShopVO.lstSite.add(omsSiteVO);
-                            }
-                        }
-                        omsPlatformVO.lstShop.add(omsShopVO);
-                }
-                omsPlatformVOS.add(omsPlatformVO);
-            }
-        }
-            return VoHelper.getSuccessResult(omsPlatformVOS);
-
+               List<ShopSiteDO> shopSiteDOS = shopSiteMapper.selectShopSiteByPlatformId(platformDO.getPlatformId());
+               if (shopSiteDOS == null || shopSiteDOS.size() == 0) {
+                   continue;
+               } else {
+                   //集合都必须先初识化
+                   omsPlatformVO.lstShop =new ArrayList<>(shopSiteDOS.size());
+                   for (ShopSiteDO shopSiteDO : shopSiteDOS) {
+                       OmsShopVO omsShopVO = new OmsShopVO();
+                       //针对速卖通的
+                       omsShopVO.shopId = shopSiteDO.getSellerId();
+                       omsShopVO.shopName = shopSiteDO.getShop();
+                       //如果站点id为空,则list为空
+                       if ("".equals(shopSiteDO.getSiteId())) {
+                           omsShopVO.lstSite = null;
+                       } else {
+                           OmsSiteVO omsSiteVO = new OmsSiteVO();
+                           omsSiteVO.siteId = shopSiteDO.getSiteId();
+                           //如果站点名称为空,则吧站点id赋值给站点名称
+                           if ("".equals(shopSiteDO.getSiteName())) {
+                               omsSiteVO.siteName = omsSiteVO.siteId;
+                           } else {
+                               omsSiteVO.siteName = shopSiteDO.getSiteName();
+                               omsShopVO.lstSite.add(omsSiteVO);
+                           }
+                       }
+                       omsPlatformVO.lstShop.add(omsShopVO);
+                   }
+                   omsPlatformVOS.add(omsPlatformVO);
+               }
+           }
+           return VoHelper.getSuccessResult(omsPlatformVOS);
+       }catch (Exception e){
+           logger.error("未知异常:",e);
+           return VoHelper.getErrorResult(CommonMessageCodeEnum.FAIL.getCode(),"未知异常");
+       }
     }
 
     @Override
