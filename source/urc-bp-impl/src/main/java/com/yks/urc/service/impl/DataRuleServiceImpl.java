de93db4c9936f057a3d50fdd38ff293b524832a8
wujianghui <wujianghui@youkeshu.com>
2018-08-07 14:14:57 +0800

    获取用户数据权限根据sysKey获取

--- source/urc-bp-impl/src/main/java/com/yks/urc/service/impl/DataRuleServiceImpl.java
+++ source/urc-bp-impl/src/main/java/com/yks/urc/service/impl/DataRuleServiceImpl.java
@@ -1037,19 +1037,27 @@ public class DataRuleServiceImpl implements IDataRuleService {
     }
 
     @Override
-    public ResultVO getDataRuleByUser(List<String> lstUserName, String operator) {
+    public ResultVO getDataRuleByUser(List<String> lstUserName, String operator,String sysKey) {
         if (!roleMapper.isAdminOrSuperAdmin(operator)) {
             throw new URCBizException("既不是超级管理员也不是业务管理员", ErrorCode.E_100003);
         }
+
+        if(StringUtility.isNullOrEmpty(sysKey)){
+            return VoHelper.getSuccessResult((Object) "sysKey为空");
+        }
+
         List<DataRuleVO> dataRuel = new ArrayList<DataRuleVO>();
         if (lstUserName != null && lstUserName.size() > 0) {
             for (int i = 0; i < lstUserName.size(); i++) {
                 UserDO userDO = new UserDO();
                 userDO.setUserName(lstUserName.get(i));
-                List<String> sysKeys = null;
-                if (roleMapper.isAdminAccount(operator)) {
+                List<String> sysKeys = new ArrayList<>();
+/*                if (roleMapper.isAdminAccount(operator)) {
                     sysKeys = userRoleMapper.getSysKeyByUser(operator);
-                }
+                }*/
+
+                sysKeys.add(sysKey);
+
                 //通过用户名得到DataRuleSysId\SysKey\SysName
                 List<DataRuleSysDO> syskeyList = dataRuleSysMapper.getDataRuleSysByUserName(userDO, sysKeys);
                 List<DataRuleSysVO> lstDataRuleSys = new ArrayList<DataRuleSysVO>();
