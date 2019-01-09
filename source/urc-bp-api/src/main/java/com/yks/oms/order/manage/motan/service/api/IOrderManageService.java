3755359b818a5e0add703147a8353a031368f07c
panyun <panyun@youkeshu.com>
2019-01-09 11:23:24 +0800

    update oms referer

---
+++ source/urc-bp-api/src/main/java/com/yks/oms/order/manage/motan/service/api/IOrderManageService.java
@@ -0,0 +1,16 @@
+package com.yks.oms.order.manage.motan.service.api;
+
+import com.yks.urc.vo.ResultVO;
+
+public interface IOrderManageService {
+
+    /**
+     * @Description :获取所有平台销售账号
+     * @Author: tangjianbo@youkeshu.com
+     * @Date: 2018/12/18 12:21
+     * @Param:
+     * @return:
+     **/
+
+    ResultVO getAllSellerId();
+}
