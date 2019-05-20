5ef5e10f2d298857b681292e2fa25e0ed486ce9c
panyun <panyun@youkeshu.com>
2019-05-20 16:43:41 +0800

    add审计config

---
+++ doc/config文件/urcenter-sj/urcenter/service_restart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export JAVA_HOME=/jdk1.8.0_151
+###启动urc-service-provider服务
+/opt/urc-service-provider/bin/shutdown.sh
+sleep 2
+cd /opt
+rm -rf /opt/urc-service-provider/
+tar -xvf urc-service-provider-assembly.tar.gz
+sleep 2
+cd urc-service-provider/bin
+./start.sh
