65aaaa6bf79e2f1c21c3fecffced1a8b1ebae7e6
panyun <yun527292121@163.com>
2020-04-03 11:55:25 +0800

    fix jenkins

--- doc/config文件/urcenter-test1/urcenter/service_restart.sh
+++ doc/config文件/urcenter-test1/urcenter/service_restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export JAVA_HOME=/jdk1.8.0_151
+
 ###启动urc-service-provider服务
 /opt/urc-service-provider/bin/shutdown.sh
 sleep 2
@@ -8,4 +9,18 @@ rm -rf /opt/urc-service-provider/
 tar -xvf urc-service-provider-assembly.tar.gz
 sleep 2
 cd urc-service-provider/bin
+./start.sh
+
+
+###启动urc-service-provider服务
+/opt/urc-mgr/bin/shutdown.sh
+sleep 2
+cd /opt
+rm -rf /opt/urc-mgr/
+tar -xvf urc-mgr-assembly.tar.gz
+sleep 2
+
+# 8002端口改为8003
+sed -in-place -e s/8002/8003/g urc-mgr/conf/motan/motan-service-config.xml
+cd urc-mgr/bin
 ./start.sh
