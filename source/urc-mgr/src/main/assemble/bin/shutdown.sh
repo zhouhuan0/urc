04b3cd5985181ec8713617ea7dd9848a568ee7ea
panyun <yun527292121@163.com>
2020-04-03 15:00:10 +0800

    fix shutdown.sh

--- source/urc-mgr/src/main/assemble/bin/shutdown.sh
+++ source/urc-mgr/src/main/assemble/bin/shutdown.sh
@@ -12,7 +12,7 @@ JAVA_OPTS="-Xss256k"
 #==============================================================================
 
 #stop Server
-$(ps -ef | grep UrcMgrStarter | awk '{print $2}' | xargs kill -9 )
+$(ps -ef | grep urc-mgr | awk '{print $2}' | xargs kill -9 )
 
-echo "Shutdown urc-Mgr is done....."
+echo "Shutdown urc-mgr is done....."
 #==============================================================================
