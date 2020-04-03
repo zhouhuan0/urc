12d32d92eb26ee42e193a17593d2a21a56d3ed75
panyun <yun527292121@163.com>
2020-04-03 15:12:04 +0800

    fix jenkins

--- cover_config.sh
+++ cover_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -rf ./source/urc-service-provider/src/main/resources_template/* ./source/urc-service-provider/src/main/resources
-cp -rf ./source/urc-service-provider/src/main/resources_template/* ./source/urc-taskScheduler/src/main/resources
-cp -rf ./source/urc-service-provider/src/main/resources_template/* ./source/urc-mgr/src/main/resources
+cp -rf ./source/conf/urc-service-provider/src/main/resources/* ./source/urc-service-provider/src/main/resources
+cp -rf ./source/conf/urc-service-provider/src/main/resources/* ./source/urc-taskScheduler/src/main/resources
+cp -rf ./source/conf/urc-service-provider/src/main/resources/* ./source/urc-mgr/src/main/resources
 cp -rf ./source/conf/urc-mgr/src/main/resources/* ./source/urc-mgr/src/main/resources
