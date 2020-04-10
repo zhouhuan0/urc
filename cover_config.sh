a600372863fa90d524b813f402748ca13ca63287
panyun <yun527292121@163.com>
2020-04-10 16:37:37 +0800

    fix start.sh

--- cover_config.sh
+++ cover_config.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+git name-rev --name-only HEAD >>  ./source/urc-mgr/src/main/resources/git.txt
+git log --pretty=format:"%an %ad %h %s" -n 1  --date=format:'%Y-%m-%d %H:%M:%S' >>  ./source/urc-mgr/src/main/resources/git.txt
+
+git name-rev --name-only HEAD >>  ./source/urc-taskScheduler/src/main/resources/git.txt
+git log --pretty=format:"%an %ad %h %s" -n 1  --date=format:'%Y-%m-%d %H:%M:%S' >>  ./source/urc-taskScheduler/src/main/resources/git.txt
+
 cp -rf ./source/conf/urc-service-provider/src/main/resources/* ./source/urc-service-provider/src/main/resources
 cp -rf ./source/conf/urc-service-provider/src/main/resources/* ./source/urc-taskScheduler/src/main/resources
 cp -rf ./source/conf/urc-service-provider/src/main/resources/* ./source/urc-mgr/src/main/resources
