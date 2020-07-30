f20d7509db24c4a7430e3ed7e5c46ee0d1d2eba8
panyun <yun527292121@163.com>
2020-07-30 20:15:54 +0800

    fix

--- source/urc-bp-api/src/main/java/com/yks/urc/sellerid/bp/api/IActMgrBp.java
+++ source/urc-bp-api/src/main/java/com/yks/urc/sellerid/bp/api/IActMgrBp.java
@@ -1,10 +1,7 @@
 package com.yks.urc.sellerid.bp.api;
 
 
-import com.yks.urc.vo.AccountInfo4Third;
-import com.yks.urc.vo.DataRuleSysVO;
-import com.yks.urc.vo.OmsPlatformVO;
-import com.yks.urc.vo.UserInfo4Third;
+import com.yks.urc.vo.*;
 
 import java.util.List;
 
@@ -13,7 +10,7 @@ public interface IActMgrBp {
 
     void saveAct(List<UserInfo4Third> lstAct);
 
-    void syncAct(String dtModifyStart, String dtModifyEnd) throws Exception;
+    void syncAct(TaskParamVO taskParamVO, String dtModifyStart, String dtModifyEnd) throws Exception;
 
     void mergeAct(List<DataRuleSysVO> lstDr);
 
